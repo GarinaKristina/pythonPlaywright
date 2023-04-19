4b08115ed480b3d25caea314b0d1d09ae437a160
Kristina <tinka_e@mail.ru>
2023-04-19 14:40:55 +0300

    first tests

--- pageObjects/MainPage.py
+++ pageObjects/MainPage.py
@@ -3,13 +3,16 @@ class MainPage:
         self.page = page
         self.search_input = page.locator("//input[@id='gh-ac']")
         self.saved_button = page.locator(".saved")
-        self.electronics_button = page.locator("//li[@class='hl-cat-nav__js-tab']//a[contains(text(),'Electronics')]")
+        self.electronics_button = page.locator("//*[@class='hl-cat-nav__js-tab']//a[contains(text(),'Electronics')]")
+        self.search_button = page.locator("#gh-btn")
+        self.busket_icon = page.locator(".gh-cart-icon")
 
-    async def navigate(self):
-       await self.page.goto("https://www.ebay.com/")
 
-    async def fillFieldAndPressEnter(self, text):
-       await self.search_input.fill(text)
-       await self.search_input.press("Enter")
+    def navigate(self):
+       self.page.goto("https://www.ebay.com/")
+
+    def fillFieldAndPressEnter(self, text):
+       self.search_input.fill(text)
+       self.search_input.press("Enter")
   
 
